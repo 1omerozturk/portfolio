f267905389dbf2ab4f4cc3f5c093f46cd84f04a9
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2025-04-28 14:20:08 +0300

    added cron function

--- portfolio_nodejs/app.js
+++ portfolio_nodejs/app.js
@@ -3,20 +3,24 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+const job = require('./service/cron')
 
 // app conect to express
 const app = express()
 
 // database connection
 connectDb()
+
+// cron func
+job.start()
+
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/uploads', express.static(path.join(__dirname, './uploads')))
 app.use('/api/user', require('./routes/userRoutes'))
 app.use('/api/admin', require('./routes/adminRoutes'))
-
 
 const PORT = process.env.PORT || 4040
 
