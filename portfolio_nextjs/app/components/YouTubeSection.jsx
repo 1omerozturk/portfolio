0f0d798c7c1c1a9bdac99af48fa0bb1ad8ecd0ef
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2026-01-10 13:47:34 +0300

    Create YouTubeSection.jsx

---
+++ portfolio_nextjs/app/components/YouTubeSection.jsx
@@ -0,0 +1,30 @@
+export default function YoutubeSection() {
+  return (
+    <section className="youtube-section">
+      <h2>YouTube Videolarım</h2>
+      <p>Paylaştığım videolara buradan ulaşabilir ve kanalıma abone olabilirsin.</p>
+
+      {/* Playlist */}
+      <div className="video-wrapper">
+        <iframe
+          src="https://www.youtube.com/embed/videoseries?list=PLk1vdjqSfYiZzQYK1NnfwAe0Lo6g5Epvf"
+          title="Ömer Öztürk YouTube Playlist"
+          frameBorder="0"
+          loading="lazy"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+
+      {/* Subscribe CTA */}
+      <a
+        href="https://www.youtube.com/@omer.ozturk?sub_confirmation=1"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="subscribe-btn"
+      >
+        🔔 YouTube Kanalıma Abone Ol
+      </a>
+    </section>
+  );
+}
